083c061dbb20cc6bb6e7efaa8a88b34e10ce60dd
iJackWilson <github@jack.lu>
2022-04-25 14:49:16 +0100

    Add Terraform state bucket

--- deploy/main.tf
+++ deploy/main.tf
@@ -6,7 +6,7 @@ terraform {
       }
     }
    backend "s3" {
-     bucket = "apples.jackwilson.uk"
+     bucket = "tfstate-ghost-website.jackwilson.uk"
      key    = "terraform.tfstate"
      region = "eu-central-1"
    }
@@ -14,4 +14,18 @@ terraform {
 
 provider "aws" {
   region  = "eu-central-1"
+}
+
+resource "aws_s3_bucket" "tfstate-ghost-website_jackwilson_uk" {
+	bucket = "tfstate-ghost-website.jackwilson.uk"
+
+	versioning {
+	    enabled = true
+	}
+	server_side_encryption_configuration {
+    	rule {
+      	  apply_server_side_encryption_by_default {
+          sse_algorithm = "AES256"
+	}
+    }
 }
