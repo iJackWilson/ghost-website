745370febe14a0e1b6f6b1caa6d6ebabb88bbcbc
iJackWilson <github@jack.lu>
2022-04-25 15:19:16 +0100

    remove state bucket

--- deploy/main.tf
+++ deploy/main.tf
@@ -16,17 +16,3 @@ provider "aws" {
   region  = "eu-central-1"
 }
 
-resource "aws_s3_bucket" "tfstate-ghost-website_jackwilson_uk" {
-	bucket = "tfstate-ghost-website.jackwilson.uk"
-
-	versioning {
-	    enabled = true
-	}
-	server_side_encryption_configuration {
-    	rule {
-      	  apply_server_side_encryption_by_default {
-          sse_algorithm = "AES256"
-	}
-     }
-  }
-}
